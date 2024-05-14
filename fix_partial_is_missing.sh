06f327f5e0b618bb737f5b9f2888239c7ef1ca18
roothider <158083651+roothider@users.noreply.github.com>
2024-05-15 03:52:14 +0800

    fix

--- fix_partial_is_missing.sh
+++ fix_partial_is_missing.sh
@@ -1,9 +1,11 @@
 #fix "/var/cache/apt/archives/partial is missing" for Sileo
 #run this script in filza installed in trollstore
 
-mkdir /var/cache
-mkdir /var/cache/locate
-mkdir -p /var/cache/apt/archives/partial
-printf "" > /var/cache/apt/archives/lock
+set JBROOT $(find /var/containers/Bundle/Application -maxdepth 1 -name ".jbroot-*")
+
+mkdir $JBROOT/var/cache
+mkdir $JBROOT/var/cache/locate
+mkdir -p $JBROOT/var/cache/apt/archives/partial
+printf "" > $JBROOT/var/cache/apt/archives/lock
 
 echo "done"
