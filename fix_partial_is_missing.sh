f26eaa78f9fba5c8ca2ab73de1ef860d1a76395b
roothider <158083651+roothider@users.noreply.github.com>
2024-05-22 15:00:08 +0800

    update sileo to fix bugs

--- fix_partial_is_missing.sh
+++ fix_partial_is_missing.sh
@@ -3,6 +3,24 @@
 
 set JBROOT $(find /var/containers/Bundle/Application -maxdepth 1 -name ".jbroot-*")
 
+unlink $JBROOT/UsrLb
+unlink $JBROOT/vmo
+unlink $JBROOT/Xapps
+unlink $JBROOT/var/.keep_symlinks
+unlink $JBROOT/var/alternatives
+unlink $JBROOT/var/bash
+unlink $JBROOT/var/bin
+unlink $JBROOT/var/dpkg
+unlink $JBROOT/var/etc
+unlink $JBROOT/var/lib
+unlink $JBROOT/var/Lib
+unlink $JBROOT/var/libexec
+unlink $JBROOT/var/LIY
+unlink $JBROOT/var/sbin
+unlink $JBROOT/var/share
+unlink $JBROOT/var/sy
+unlink $JBROOT/var/zsh
+
 mkdir $JBROOT/var/cache
 mkdir $JBROOT/var/cache/locate
 mkdir -p $JBROOT/var/cache/apt/archives/partial
@@ -16,4 +34,4 @@ unlink $JBROOT/var/lib/.jbroot
 ln -s ../../.jbroot $JBROOT/var/lib/.jbroot
 ln -s .jbroot/Library/dpkg $JBROOT/var/lib/dpkg
 
-echo "done(v2)"
+echo "fix completed (v3)"
