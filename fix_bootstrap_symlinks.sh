17f1b6b49475ef4b379072e81a7ec62935f9d5a6
roothider <158083651+roothider@users.noreply.github.com>
2026-02-23 13:05:11 +0800

    Update fix_bootstrap_symlinks.sh

--- fix_bootstrap_symlinks.sh
+++ fix_bootstrap_symlinks.sh
@@ -3,6 +3,18 @@
 
 set JBROOT $(find /var/containers/Bundle/Application -maxdepth 1 -name ".jbroot-*")
 
+if test -z "$JBROOT"
+	echo "error: jbroot dir not found"
+	exit 1
+end
+
+rm $JBROOT/.jbroot 2>/dev/null || true
+rm $JBROOT/bin/.jbroot 2>/dev/null || true
+rm $JBROOT/usr/bin/.jbroot 2>/dev/null || true
+
+ln -sf . $JBROOT/.jbroot
+ln -sf ../.jbroot $JBROOT/bin/.jbroot
+ln -sf ../../.jbroot $JBROOT/usr/bin/.jbroot
 ln -sf .jbroot/usr/bin/dash $JBROOT/bin/sh
 ln -sf .jbroot/usr/bin/dash $JBROOT/usr/bin/sh
 
